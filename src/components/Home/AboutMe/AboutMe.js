a4d9ecae2698abaa307911c3b05e029642cedc9a
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-19 15:47:03 -0700

    create about me Page component

---
+++ src/components/Home/AboutMe/AboutMe.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+
+const AboutMe = () => {
+    return(
+        <div>
+            <Navigation />
+            About Me.
+        </div>
+    )
+}
+
+export default AboutMe;
