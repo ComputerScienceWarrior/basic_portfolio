85ff244fef335ea574c56a4698e504352836a822
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-19 22:17:09 -0700

    adds navigation across application from this 1 file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AboutMe from './components/Home/AboutMe/AboutMe';
 import Projects from './components/Home/Projects/Projects'
 import Home from './components/Home/Home';
 import Resume from './components/Home/Resume/Resume';
+import Navigation from './components/Home/Navigation/Navigation';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <ChakraProvider>
       <BrowserRouter>
+      <Navigation />
         <Routes>
           <Route path="/" index element={<Home />}/>
           <Route path="/about_me" element={<AboutMe />}/>
