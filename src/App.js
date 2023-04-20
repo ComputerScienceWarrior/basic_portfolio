a37e639ec356951ef764b2606496b6d13cc61ffa
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-19 18:28:00 -0700

    wraps project with Chakra UI

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import AboutMe from './components/Home/AboutMe/AboutMe';
 import Projects from './components/Home/Projects/Projects'
 import Home from './components/Home/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" index element={<Home />}/>
-        <Route path="/about_me" element={<AboutMe />}/>
-        <Route path="/projects" element={<Projects />}/>
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" index element={<Home />}/>
+          <Route path="/about_me" element={<AboutMe />}/>
+          <Route path="/projects" element={<Projects />}/>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
