90aff3b716c8f4476b486671fd5ebb78f223f6a4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-19 18:47:16 -0700

    creates Projects component to render list of projects

---
+++ src/components/Home/Projects/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+import Project from "../Project/Project";
+
+const Projects = () => {
+    return(
+        <div>
+            <Navigation />
+            Projects!
+        </div>
+    )
+}
+
+export default Projects;
