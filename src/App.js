c4f623054205a5756865d19e7f8f70b8bbb73592
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-19 20:08:15 -0700

    adds /login route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AboutMe from './components/Home/AboutMe/AboutMe';
 import Projects from './components/Home/Projects/Projects'
 import Home from './components/Home/Home';
+import Login from './components/Session/Login/Login'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" index element={<Home />}/>
           <Route path="/about_me" element={<AboutMe />}/>
           <Route path="/projects" element={<Projects />}/>
+          <Route path="/login" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
