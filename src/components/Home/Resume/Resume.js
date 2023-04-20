6f5da87505d439dbf9a3b37c33828cf39c38e333
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-19 22:05:23 -0700

    creates basic Resume component

---
+++ src/components/Home/Resume/Resume.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+
+const Resume = () => {
+    return(
+        <div>
+            <Navigation />
+            Resume Page!
+        </div>
+    )
+}
+
+export default Resume;
