2f2dff25440ce315040f5cf7a4a20914e0c7e32e
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-19 18:27:22 -0700

    adds style to navigation bar

--- src/components/Home/Navigation/Navigation.js
+++ src/components/Home/Navigation/Navigation.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import { Box } from '@chakra-ui/react'
 
 const Navigation = () => {
 
     return(
-        <>
-            <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about_me">About Me</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
+        <Box>
+            <nav style={{textDecoration: 'none', backgroundColor: 'navy'}}>
+                <ul className="" style={{display: 'flex', justifyContent: 'space-evenly', listStyleType: 'none'}}>
+                    <li><Link className="text-white font-bold" style={{textDecoration: 'none', textColor: 'white'}} to="/">Home</Link></li>
+                    <li><Link className="text-white font-bold" style={{textDecoration: 'none', textColor: 'white'}} to="/about_me">About Me</Link></li>
+                    <li><Link className="text-white font-bold" style={{textDecoration: 'none', textColor: 'white'}} to="/projects">Projects</Link></li>
                 </ul>
             </nav>
             <Outlet />
-        </>
+        </Box>
     )
 }
 
