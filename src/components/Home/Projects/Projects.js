82b322b5e0bce3af534db5c51fde0027581d87cd
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-20 22:01:18 -0700

    iterates over project list constant data to display project cards

--- src/components/Home/Projects/Projects.js
+++ src/components/Home/Projects/Projects.js
@@ -1,13 +1,20 @@
 import React from "react";
-import Navigation from "../Navigation/Navigation";
+import { Box, Text } from "@chakra-ui/react"
 import Project from "../Project/Project";
+import { ProjectData1 } from "../constants/constants";
 
 const Projects = () => {
     return(
-        <div>
-            <Navigation />
-            Projects!
-        </div>
+        <Box>
+            <Box>
+                <Text className="text-blue-500 font-bold text-center text-2xl">Project Showcase</Text>
+            </Box>
+            <Box display={'flex'} justifyContent={'space-evenly'}>
+                {ProjectData1.map((project, key) => {
+                    return <Project key={key} title={project.title} languages={project.languages} imageUrl={project.imageUrl} />
+                })}
+            </Box>
+        </Box>
     )
 }
 
