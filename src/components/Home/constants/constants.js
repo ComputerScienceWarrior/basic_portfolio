3be74d5c2531f064c34d32029bf9ad56213bcfa8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-20 22:02:03 -0700

    creates project data to import and iterate over

---
+++ src/components/Home/constants/constants.js
@@ -0,0 +1,27 @@
+import JobSearch from '../../JobSearch.png'
+import Math from '../../Math.jpeg'
+import CRM from '../../CRM.webp'
+
+export const ProjectData1 = [
+    { title: 'Job Search', 
+      languages: 'Ruby', 
+      githubUrl: 'https://github.com/ComputerScienceWarrior/job_search',
+      imageUrl: JobSearch,
+      websiteUrl: '',
+      videoDemoUrl: '',
+    },
+    { title: 'Math Skills', 
+      languages: 'React Native', 
+      githubUrl: 'https://github.com/ComputerScienceWarrior/LearningManagementSystem',
+      imageUrl: Math,
+      websiteUrl: '',
+      videoDemoUrl: ''
+    },
+    { title: 'CRM', 
+      languages: 'React JS, Ruby on Rails', 
+      githubUrl: 'https://github.com/ComputerScienceWarrior/CRM',
+      imageUrl: CRM,
+      websiteUrl: '',
+      videoDemoUrl: ''
+    }
+]
