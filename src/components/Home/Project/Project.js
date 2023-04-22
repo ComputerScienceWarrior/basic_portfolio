4b55bb50035c0c47dd21ea43507cb412d7bc9e7e
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-04-22 11:38:48 -0700

    properly structure <Project/> card

--- src/components/Home/Project/Project.js
+++ src/components/Home/Project/Project.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { Box, Text, Image } from '@chakra-ui/react'
+import Youtube from '../../Youtube.png'
+import Github from '../../Github.png'
 
 const Project = (props) => {
+
     return(
-        <div>
-            <div>{props.title}</div>
-            <img src={props.imgUrl} />
-            <div>{props.websiteUrl}</div>
-            <div>{props.githubRepo}</div>
-            <div>{props.videoDemoUrl}</div>
-        </div>
+        <Box bg={'gray.200'} w={'40'} h={'40'} m={'20'}>
+            <Box>
+                <Text bg={'navy'} className="text-white text-center font-bold text-xl">{props.title}</Text>
+                <Image src={props.imageUrl} w='full' h='150px' />
+                <div>{props.websiteUrl}</div>
+            </Box>
+            <Box><Text maxH={'30px'} overflowY={'scroll'} bg={'black'} className="text-white font-bold text-xl text-center">{props.languages}</Text></Box>
+            <Box display={'flex'} justifyContent={'space-evenly'} bg={'white'} border={'solid'} borderWidth={'thin'}>
+                <a href={props.videoDemoUrl} target="_blank">
+                    <Image src={Youtube} w='1/2' h='35px' />
+                </a>
+                <a href={props.githubUrl} target="_blank">
+                    <Image src={Github} w='1/2' h='35px' />
+                </a>
+            </Box>
+        </Box>
+            
     )
 }
 
