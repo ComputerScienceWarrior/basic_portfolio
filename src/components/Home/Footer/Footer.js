8c0315d558a074e377c1485e5eeb528dc3c5dd46
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-05-02 15:59:59 -0700

    Create basic Footer component

---
+++ src/components/Home/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box } from "@chakra-ui/react"
+
+const Footer = () => {
+
+    return(
+        <Box className="fixed bottom-0 h-20 w-full" bgColor={'navy'}>
+            Footer!
+        </Box>
+    )
+}
+
+export default Footer;
