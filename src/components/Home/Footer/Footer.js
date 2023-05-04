88aa9cd239377a0af71520ea92fc6a043b3aad80
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-05-03 17:34:15 -0700

    updates footer info

--- src/components/Home/Footer/Footer.js
+++ src/components/Home/Footer/Footer.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Box } from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react"
 
 const Footer = () => {
 
     return(
-        <Box className="fixed bottom-0 h-20 w-full" bgColor={'navy'}>
-            Footer!
+        <Box className="text-white flex justify-evenly fixed bottom-0 h-20 w-full items-center" bgColor={'navy'}>
+            <Box>
+                <Text><a style={{color: 'yellow'}} target="_blank" href="https://www.linkedin.com/in/james-ryan-stampley/">My LinkedIn</a></Text>
+                <Text><a style={{color: 'yellow'}} target="_blank" href="https://github.com/ComputerScienceWarrior">My Github Page</a></Text>
+            </Box>
+
+            <Box>
+                <Text>jamesryanstampley@gmail.com</Text>
+                <Text>(602)-860-2056</Text>
+            </Box>
         </Box>
     )
 }
