c49eaa3f0b6fb87cd55d8f4e234b334c5148ccb3
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-05-04 06:45:28 -0700

    use Box instead of div

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Box } from "@chakra-ui/react"
 
 const Home = () => {
     return(
-        <div>
+        <Box>
             Home Page!
-        </div>
+        </Box>
     )
 }
 
